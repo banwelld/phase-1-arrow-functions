ec5af348fdafb4c1688ac81f97e9ebe7ed4731cc
Dave Banwell <dave@davebanwell.com>
2024-04-20 18:04:11 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(divNum1, divNum2) {
+    let ans = divNum1 / divNum2;
+    return ans;
+}
+
+const square = numToSq => (numToSq ** 2);
+
+const add = (addNum1, addNum2) => (addNum1 + addNum2);
